e31ead61c2d98633b5789f9da8cb9c0d60a7bca3
Shubham Varshney <shubhamvarshneycsn@gmail.com>
2025-07-17 11:32:58 +0530

    Auto commit on 17-07-2025 11:32:58.48

--- git_auto_push.bat
+++ git_auto_push.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /d "%~dp0"
-git status > git_status_log.txt
 git add .
-git commit -m "Auto commit on %date% %time%" >> git_status_log.txt 2>&1
-git push >> git_status_log.txt 2>&1
+git commit -m "Auto commit on %date% %time%"
+git push
 pause
