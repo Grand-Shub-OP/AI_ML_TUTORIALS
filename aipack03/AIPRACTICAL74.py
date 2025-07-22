6a292757ae61c6c309ddee6f94d4d76267d071ff
Shubham Varshney <shubhamvarshneycsn@gmail.com>
2025-07-22 11:04:51 +0530

    Auto commit on 22-07-2025 11:04:50.95

--- aipack03/AIPRACTICAL74.py
+++ aipack03/AIPRACTICAL74.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3,4,5],[1,2,3,4,5],'bo-',label='team 1',linewidth=2)
+plt.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], 'bo-', label='team 1', linewidth=2)
+plt.plot([1, 2, 3, 4, 5], [1, 4, 9, 16, 25], 'rs-', label='team 2', linewidth=4)
 
-plt.plot([1,2,3,4,5],[1,2,3,4,5],'rs-', label='team 2', linewisth=10)
-
-plt.axis([0,6,8,26])
-
-plt.legend(loc="upper left")
-
+plt.axis([0, 6, 0, 26])
+plt.legend(loc="lower right")
 plt.show()
