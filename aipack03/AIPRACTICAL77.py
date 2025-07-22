4133d3db98fa7379ea37ea0d6b54ac9ace16108b
Shubham Varshney <shubhamvarshneycsn@gmail.com>
2025-07-22 11:48:00 +0530

    Auto commit on 22-07-2025 11:48:00.62

---
+++ aipack03/AIPRACTICAL77.py
@@ -0,0 +1,16 @@
+#example-7
+#----------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#evenly sampled time at 200ms intervals
+t=np.arange(0.0, 5.0, 0.2)
+print(t)
+
+#red stars , blue squares , green dots
+plt.plot( t, t, 'r*-',
+                t,t+3, 'bs-',
+                t,t+6, 'go-',
+                markersize=7)
+plt.show()
