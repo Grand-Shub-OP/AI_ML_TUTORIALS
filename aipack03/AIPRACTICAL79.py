018c083f410765da90bee75c6f22d6374fec21f9
Shubham Varshney <shubhamvarshneycsn@gmail.com>
2025-07-24 11:26:13 +0530

    Auto commit on 24-07-2025 11:26:13.13

--- aipack03/AIPRACTICAL79.py
+++ aipack03/AIPRACTICAL79.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #plotting with default settings
-x=np.linspace(-np.pi, np.pi, num 255, endpoint=true)
-print("X=",X)
+x=np.linspace(-np.pi, np.pi, 3 , endpoint=True)
+print("X=",x)
 
-S=np.sin(X)
+S=np.sin(x)
 C=np.cos(x)
 
-plt.plot(X,S,label='sin curve')
-plt.plot(X,c,label='cos curve')
-plt.grid(true)
-plt/legend(loc+"upper left")
+plt.plot(x,S,label='sin curve')
+plt.plot(x,C,label='cos curve')
+plt.grid(True)
+plt.legend(loc="upper left")
 plt.show()
